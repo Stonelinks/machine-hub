40223b936bd6b8c8afc39a799b88ec031a0ade46
Lucas Doyle <lucas.doyle@samsara.com>
2021-10-02 01:47:33 -0700

    back to normal

--- cncjs/cncjs/src/server/config/settings.production.js
+++ cncjs/cncjs/src/server/config/settings.production.js
@@ -18,7 +18,7 @@ export default {
     assets: {
         app: {
             routes: [ // with trailing slash
-                urljoin('/cncjs/', '/'),
+                urljoin(publicPath, '/'),
                 '/' // fallback
             ],
             path: path.resolve(__dirname, '..', '..', 'app'),
