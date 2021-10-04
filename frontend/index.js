eb28643e5629769affa6af31243ee0efbdb482f8
Lucas Doyle <lucas.doyle@samsara.com>
2021-10-03 21:13:09 -0700

    asdlkjsjld

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,13 @@ var http = require('http');
 var express = require('express');
 
 var PORT = 8081;
+var HOST = "0.0.0.0";
 
 var app = express();
 var server = http.createServer(app);
 
 app.use(express.static(__dirname + '/static'))
-server.listen(PORT, function() {
+server.listen(PORT, HOST, function() {
 	console.log("server is listening on port", PORT);
 });
 
