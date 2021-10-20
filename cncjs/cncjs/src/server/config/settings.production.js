74f26288dbb60f27e7996762fc544fb776ef9bdb
Lucas Doyle <lucas.doyle@samsara.com>
2021-10-20 16:21:27 -0700

    give up on trying to run cncjs on a different path

--- cncjs/cncjs/src/server/config/settings.production.js
+++ cncjs/cncjs/src/server/config/settings.production.js
@@ -18,7 +18,7 @@ export default {
     assets: {
         app: {
             routes: [ // with trailing slash
-                urljoin('/cncjs/', '/'),
+                urljoin(publicPath, '/'),
                 '/' // fallback
             ],
             path: path.resolve(__dirname, '..', '..', 'app'),
@@ -29,7 +29,7 @@ export default {
         enable: false, // disable backend service in production
         host: 'localhost',
         port: 80,
-        route: 'cncjs/api/'
+        route: 'api/'
     },
     cluster: {
         // note. node-inspector cannot debug child (forked) process
