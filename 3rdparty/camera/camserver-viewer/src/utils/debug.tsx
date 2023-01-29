b6c675c003ca08d33ae6a64fee7d7fe8b631b327
Lucas Doyle <lucas.doyle@samsara.com>
2023-01-29 13:37:30 -0800

    rm camserver

--- 3rdparty/camera/camserver-viewer/src/utils/debug.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DEBUG = true;
-
-const Debug: React.FC<{
-  d: any;
-}> = ({ d }) => (DEBUG ? <pre>{JSON.stringify(d, null, 2)}</pre> : null);
-
-export default Debug;
--- 3rdparty/camera/src/common/json.ts
+++
@@ -1,7 +0,0 @@
-export type JsonPrimitive = string | number | boolean | null;
-export type JsonValue = JsonPrimitive | JsonObject | JsonArray;
-
-export type JsonObject = { [member: string]: JsonValue };
-export interface JsonArray extends Array<JsonValue> {}
-
-export type JsonSerializable = JsonArray | JsonObject;
